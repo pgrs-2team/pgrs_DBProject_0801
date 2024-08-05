6d0a0db096ce93bb664c9a5872b8917223aa35a5
leebs0521 <leebs0521@gmail.com>
2024-08-05 10:33:34 +0900

    feat: databsae connect

---
+++ src/com/programmers/demo/common/DBUtil.java
@@ -0,0 +1,34 @@
+package com.programmers.demo.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/programmers";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+    private static DBUtil instance;
+    private Connection connection;
+
+    private DBUtil() {
+        try {
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("데이터베이스 연결 실패", e);
+        }
+    }
+
+    public static DBUtil getInstance() {
+        if (instance == null) instance = new DBUtil();
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+}
+
+
